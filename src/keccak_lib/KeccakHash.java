579d2665cdc5d4bf6172f5802714ccedcd00446f
JacentyI1 <jacek.kwiecinski@student.put.poznan.pl>
2023-04-19 22:38:04 +0200

    We use Long instead of BigInteger. I think we deal mainly with 64-bits.

--- src/keccak_lib/KeccakHash.java
+++ src/keccak_lib/KeccakHash.java
@@ -1,4 +1,25 @@
 package keccak_lib;
-
+/**
+ * The Keccak hash function, with a hash-compatible interface.*/
 public class KeccakHash {
+    KeccakHash(){
+        //ToDo: __init__
+    }
+    public Object repr(){
+        //ToDo: Return printable representation of KeccakHash
+        return null;
+    }
+    public void copy(){
+        //ToDo: deepcopy() xd
+    }
+    public void update(){
+        //ToDo: update()
+    }
+    public void hexdigest(){}
+    /**
+     * Returns a factory function for the given bitrate, sponge capacity and output length.
+     * The function accepts an optiona initial input, ala hashlib. */
+    static public void preset(){
+
+    }
 }
